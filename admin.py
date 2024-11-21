892956a672eace651af7b03fa31ce768ffc65bd1
Jakub Piotrowski <kubapiot@gmail.com>
2024-11-21 16:48:33 +0100

    done

--- admin.py
+++ admin.py
@@ -1,7 +1,27 @@
+import requests
 import streamlit as st
 import pandas as pd
 import os
+import json
 from st_aggrid import AgGrid, GridOptionsBuilder, DataReturnMode
+
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google.oauth2.service_account import Credentials
+
+service_account_info = json.loads(st.secrets["SERVICE_ACCOUNT_JSON"])
+
+
+# Authenticate using the service account JSON
+SCOPES = ['https://www.googleapis.com/auth/drive']
+credentials = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+service = build('drive', 'v3', credentials=credentials)
+
+
+# Specify the folder ID
+FOLDER_ID = '1dWsz0Er13-odiXI4OQ6N_Exqg0UVxTS3'
+
+# Query to get files in the specific folder
 
 
 st.set_page_config(layout="wide")
@@ -14,7 +34,26 @@ col1, col2 = st.columns(2)
 # Load the CSV data
 df = pd.read_csv('chatgpt_output.csv')
 
+# Define function to get image URLs based on SiteID
+def get_images(siteid):
+    query = f"'{FOLDER_ID}' in parents and name contains '{siteid}' and trashed = false"
 
+    # Retrieve files
+    results = service.files().list(
+        q=query,
+        fields="files(id, name)"
+    ).execute()
+
+    files = results.get('files', [])
+    image_urls = []
+
+    if files:
+        for file in files:
+            #file_url = f"https://drive.google.com/file/d/{file['id']}/view"
+            file_url = f"https://drive.google.com/uc?export=view&id={file['id']}"
+
+            image_urls.append((file['name'], file_url))
+    return image_urls
 
 # Display site grid with AgGrid
 st.subheader("All Sites")
@@ -64,13 +103,14 @@ if len(selected_rows) > 0:
 
     # Search for and display images matching the SiteID
     with col2:
-        images_found = False
-        for filename in os.listdir(image_folder):
-            if filename.startswith(str(site_id) + '_'):
-                image_path = os.path.join(image_folder, filename)
-                st.image(image_path, caption=filename)
-                images_found = True
+        st.subheader(f"Images for SiteID: {site_id}")
+        image_urls = get_images(site_id)
 
-    if not images_found:
-        st.warning(f"No images found for SiteID: {site_id}")
+        if image_urls:
+            for name, url in image_urls:
+                response = requests.get(url)
+                st.image(response.content)
+        else:
+            st.warning(f"No images found for SiteID: {site_id}")
+
 
